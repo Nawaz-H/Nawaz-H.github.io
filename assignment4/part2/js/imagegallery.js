e2bb2b811922342487daf2c6fdf45be072ab6036
NawazH <haleema.nawaz@dcmail.ca>
2026-04-10 19:58:49 -0400

    added details
    created array of objects called images in the js file

--- assignment4/part2/js/imagegallery.js
+++ assignment4/part2/js/imagegallery.js
@@ -3,3 +3,11 @@ const thumbBar = document.querySelector(".thumb-bar");
 
 const btn = document.querySelector("button");
 const overlay = document.querySelector(".overlay");
+
+const images = [
+      { filename: "pic1", alt: "Closeup of a human eye"},
+      { filename: "pic2", alt: "Rock that looks like a wave"},
+      { filename: "pic3", alt: "Purple and white pansies"},
+      { filename: "pic4", alt: "Section of wall from a pharoah's tomb"},
+      { filename: "pic5", alt: "Large moth on a leaf"}
+   ];
